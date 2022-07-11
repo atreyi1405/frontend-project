4a5ba5aa39ecd88d3aa9a4777698b904065cf4b1
atreyi <ranjanatreyi@gmail.com>
2022-07-11 18:06:16 +0100

    modified e2e welcome text

--- ezlu-ng/e2e/src/app.e2e-spec.ts
+++ ezlu-ng/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('ezlu-ng app is running!');
+    expect(page.getTitleText()).toEqual('TranscriBot app is running!');
   });
 
   afterEach(async () => {
